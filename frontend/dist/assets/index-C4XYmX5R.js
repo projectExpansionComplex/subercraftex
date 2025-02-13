db78c579b8fbb74387401f1b1881fabb959694cf
Mohamad <msiysinyuy@gmail.com>
2025-02-13 12:03:17 +0100

    updates on routes

---
+++ frontend/dist/assets/index-C4XYmX5R.js
@@ -0,0 +1,1 @@
+import{f as r,g as t}from"./index-CN4mytNG.js";var o=r();const e=t(o);export{o as r,e as v};
--- frontend/dist/assets/index-Rc2U_xE8.js
+++
@@ -1,1 +0,0 @@
-import{f as r,g as t}from"./index-DuCqjSN_.js";var o=r();const e=t(o);export{o as r,e as v};
