a9f5dacd5cb0d7cc92e29b8b0c95d8a9a0dcade0
Mohamad <msiysinyuy@gmail.com>
2025-07-03 14:14:21 +0100

    Fix: Import all models for cleanup and ensure unique test user

--- backend/config/jest.setup.js
+++ backend/config/jest.setup.js
@@ -4,6 +4,17 @@ const app = require('../src/server');
 const User = require('../models/userModel');
 const bcrypt = require('bcryptjs');
 
+// Import all models for cleanup
+const Announcement = require('../models/Announcement');
+const Category = require('../models/categoryModel');
+const ComingSoon = require('../models/ComingSoon');
+const Coupon = require('../models/discountAndCouponModel');
+const Product = require('../models/productModel');
+const Shipping = require('../models/shippingAndDeliveryModel');
+const Subscription = require('../models/Subscription');
+const Tag = require('../models/tagModel');
+const Wishlist = require('../models/wishlistModel');
+
 let globalToken;
 let globalUser;
 
@@ -20,6 +31,7 @@ beforeAll(async () => {
     password: hashedPassword,
     user_type: 'individual',
     role: 'admin',
+    username: 'testuser' + Date.now(), // Ensure unique username
   });
 
   const res = await request(app)
@@ -36,6 +48,7 @@ afterEach(async () => {
   await User.deleteMany({});
   await Announcement.deleteMany({});
   await Category.deleteMany({});
+  await ComingSoon.deleteMany({});
   await Coupon.deleteMany({});
   await Product.deleteMany({});
   await Shipping.deleteMany({});
